72c2487d941dfc602dd8dc04cb8624620dc5b9bb
KubrakovDmitry <kubrakoff.dmitry@yandex.ru>
2025-07-18 03:24:38 +0300

    Added Dockerfile

--- render_with_functions.py
+++ render_with_functions.py
@@ -259,4 +259,4 @@ def render_graph(upload_contents, upload_filename, clickData, reset_clicks,
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host="0.0.0.0", port=8050)
